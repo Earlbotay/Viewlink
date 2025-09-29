91308e5d7b3bfd324187119f658eb093e0302d7f
Earlbotay <earltiktokofficial09@gmail.com>
2025-09-29 11:06:33 +0800

    Delete setup.sh

--- setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Kemas kini dan pasang kebergantungan sistem
-sudo apt-get update
-sudo apt-get install -y chromium-browser chromium-chromedriver
-
-# Pasang kebergantungan Python
-pip install -r requirements.txt
